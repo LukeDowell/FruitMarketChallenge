90f881be7f5a42f73ebd56c1d487c034896b93aa
Luke Dowell <lukedowell@gmail.com>
2015-07-21 17:02:13 -0500

    Second commit

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 $(document).ready(function(){
 
-	var myUser = new User();
-
-	$("#allthefruits").on("click", ".buy", function(){
-		var fruitname = $(this).parent().find(".fruitname").text();
-		var fruit = findFruit(fruitname);
-		myUser.buyFruit(fruit);
-		console.log(myUser);
-	});
-
 	fruits.push(new Fruit("Apple"));
 	fruits.push(new Fruit("Banana"));
 	fruits.push(new Fruit("Orange"));
 	fruits.push(new Fruit("Pear"));
+	updateUI();
+	$("body").on("click", ".buy", function(){
+		var fruitname = $(this).prev().prev().text();
+		var fruit = findFruit(fruitname);
+		myUser.buyFruit(fruit);
+		updateUI();
+	});
 
 	setInterval(function(){
+		//Scramble our prices
 		for(var i = 0; i < fruits.length; i++) {
 			var currentPrice = fruits[i].currentPrice;
 			fruits[i].currentPrice = randomChange(currentPrice);
 		}
-	}, 15000);
+
+		//Update our ui
+		updateUI();
+	}, 3000);
 });
 
+var myUser = new User(); //Our lonely user
 var fruits = []; //Our different kinds of fruit
 
 /**
@@ -32,16 +35,16 @@ var fruits = []; //Our different kinds of fruit
 function User(){
 	this.inventory = [];
 	this.cash = 100.00;
-	this.hasFruit = function(fruit){
+	this.getInventoryItem = function(fruit){
 		for(var i = 0; i < this.inventory.length; i++) {
 			if(this.inventory[i].fruit.name === fruit.name) {
-				return true; //Yes fruit
+				return this.inventory[i];
 			}
 		}
-		return false; //No fruit
+		return null; //No fruit
 	};
 	this.buyFruit = function(fruit){
-		if(this.hasFruit(fruit)){
+		if(this.getInventoryItem(fruit) != null){
 			//we have fruit
 			for(var i = 0; i < this.inventory.length; i++) {
 				var currentItem = this.inventory[i];
@@ -56,6 +59,17 @@ function User(){
 			this.inventory.push(item);
 			this.cash -= fruit.currentPrice;
 		}
+	}
+	this.getAvgPrice = function(fruit) {
+		var item = this.getInventoryItem(fruit);
+		if(item != null) {
+			var totalPrice = 0;
+			item.history.map(function(item) {
+				totalPrice += item;
+			});
+			return totalPrice / item.history.length;
+		}
+		return 0;
 	}
 }
 
@@ -126,4 +140,45 @@ function randomChange(price){
 	}
 	console.log(price);
 	return price;
+}
+
+/**
+ * Pushes our values out to the DOM
+ */
+function updateUI() {
+	//Get our fruits
+	var apple = findFruit("Apple");
+	var orange = findFruit("Orange");
+	var banana = findFruit("Banana");
+	var pear = findFruit("Pear");
+
+	//Update apple related stuff
+	var appleItem = myUser.getInventoryItem(apple);
+	if(appleItem != null) {
+		$("#avgApplePrice").text(myUser.getAvgPrice(apple));
+		$("#appleBadge").text(appleItem.history.length);
+	}
+	$("#applePrice").text(apple.currentPrice);
+
+	var orangeItem = myUser.getInventoryItem(orange);
+	if(orangeItem != null) {
+		$("#avgOrangePrice").text(myUser.getAvgPrice(orange));
+		$("#orangeBadge").text(orangeItem.history.length);
+	}
+	$("#orangePrice").text(orange.currentPrice);
+
+	var bananaItem = myUser.getInventoryItem(banana);
+	if(bananaItem != null) {
+		$("#avgBananaPrice").text(myUser.getAvgPrice(banana));
+		$("#bananaBadge").text(bananaItem.history.length);
+	}
+	$("#bananaPrice").text(apple.currentPrice);
+
+	var pearItem = myUser.getInventoryItem(pear);
+	if(pearItem != null) {
+		$("#avgPearPrice").text(myUser.getAvgPrice(pear));
+		$("#pearBadge").text(pearItem.history.length);
+	}
+	$("#pearPrice").text(pear.currentPrice);
+
 }
