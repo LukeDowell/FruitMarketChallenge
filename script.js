837e64ab165642637531524afbbda8de62af115d
Luke Dowell <lukedowell@gmail.com>
2015-07-22 10:14:36 -0500

    Added sell functionality. Fixed some minor bugs and added a little styling to the user panel.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	fruits.push(new Fruit("Orange"));
 	fruits.push(new Fruit("Pear"));
 	updateUI();
+	//When our user clicks any buy button
 	$("body").on("click", ".buy", function(){
 		var fruitname = $(this).prev().prev().text();
 		var fruit = findFruit(fruitname);
@@ -12,7 +13,16 @@ $(document).ready(function(){
 		updateUI();
 	});
 
+	//When our user clicks any sell button
+	$("body").on("click", ".sell", function() {
+		var fruitName = $(this).val();
+		var fruit = findFruit(fruitName);
+		myUser.sellFruit(fruit, 1);
+		updateUI();
+	});
+
 	setInterval(function(){
+
 		//Scramble our prices
 		for(var i = 0; i < fruits.length; i++) {
 			var currentPrice = fruits[i].currentPrice;
@@ -21,7 +31,7 @@ $(document).ready(function(){
 
 		//Update our ui
 		updateUI();
-	}, 3000);
+	}, 15000);
 });
 
 var myUser = new User(); //Our lonely user
@@ -37,40 +47,54 @@ function User(){
 	this.cash = 100.00;
 	this.getInventoryItem = function(fruit){
 		for(var i = 0; i < this.inventory.length; i++) {
-			if(this.inventory[i].fruit.name === fruit.name) {
+			var currentItemFruitName = this.inventory[i].fruit.name.toLowerCase();
+			if(currentItemFruitName == fruit.name.toLowerCase()) {
 				return this.inventory[i];
 			}
 		}
 		return null; //No fruit
 	};
 	this.buyFruit = function(fruit){
-		if(this.getInventoryItem(fruit) != null){
-			//we have fruit
-			for(var i = 0; i < this.inventory.length; i++) {
-				var currentItem = this.inventory[i];
-				if(currentItem.fruit.name === fruit.name) { //We have found existing fruit
-					currentItem.history.push(fruit.currentPrice);
-					this.cash -= fruit.currentPrice;
+		if(this.cash >= fruit.currentPrice) {
+			if(this.getInventoryItem(fruit) != null){
+				//we have fruit
+				for(var i = 0; i < this.inventory.length; i++) {
+					var currentItem = this.inventory[i];
+					if(currentItem.fruit.name === fruit.name) { //We have found existing fruit
+						currentItem.history.push(fruit.currentPrice);
+						currentItem.quantity++;
+						this.cash -= fruit.currentPrice;
+					}
 				}
+			} else{
+				var item = new InventoryItem(fruit);
+				item.history.push(fruit.currentPrice);
+				this.inventory.push(item);
+				this.cash -= fruit.currentPrice;
 			}
-		} else{
-			var item = new Inventoryitem(fruit);
-			item.history.push(fruit.currentPrice);
-			this.inventory.push(item);
-			this.cash -= fruit.currentPrice;
 		}
-	}
+	};
+	this.sellFruit = function(fruit, quantity) {
+		var fruitItem = this.getInventoryItem(fruit);
+		if(fruitItem != null && fruitItem.quantity >= quantity) {
+			//We can sell fruit
+			for(var i = 0; i < quantity; i++) {
+				fruitItem.quantity--;
+				this.cash += fruit.currentPrice;
+			}
+		}
+	};
 	this.getAvgPrice = function(fruit) {
 		var item = this.getInventoryItem(fruit);
+		var totalPrice = 0;
 		if(item != null) {
-			var totalPrice = 0;
 			item.history.map(function(item) {
 				totalPrice += item;
 			});
-			return totalPrice / item.history.length;
+			return (totalPrice / item.history.length).toFixed(2);
 		}
-		return 0;
-	}
+		return totalPrice;
+	};
 }
 
 /**
@@ -79,10 +103,10 @@ function User(){
  * @param fruit
  * @constructor
  */
-function Inventoryitem(fruit){
+function InventoryItem(fruit){
 	this.history = [];
 	this.fruit = fruit;
-	//this.quantity = 1;
+	this.quantity = 1;
 }
 
 /**
@@ -92,7 +116,7 @@ function Inventoryitem(fruit){
  */
 function Fruit(name) {
 	this.name = name;
-	this.currentPrice = randomNumber(0.5, 9.99);
+	this.currentPrice = randomNumber(1, 9);
 }
 
 /**
@@ -100,7 +124,7 @@ function Fruit(name) {
  * @param min
  * 		Inclusive
  * @param max
- * 		Exclusive
+ * 		Inclusive
  * @returns {number}
  */
 function randomNumber(min, max) {
@@ -121,12 +145,25 @@ function findFruit(fruitname){
 }
 
 /**
+ * Checks to see if our user can afford any of the fruit
+ */
+function canAffordFruit() {
+	var canAfford = false;
+	for(var i = 0; i < fruits.length; i++) {
+		if(fruits[i].currentPrice < myUser.cash) {
+			canAfford = true;
+		}
+	}
+	return canAfford;
+}
+
+/**
  * Modifies the price of our fruit.
  * @param price
  * @returns {*}
  */
 function randomChange(price){
-	number = randomNumber(0,1);
+	var number = randomNumber(0,1);
 	if (number === 0){
 		price += 0.50;
 		if(price > 9.99){
@@ -146,6 +183,20 @@ function randomChange(price){
  * Pushes our values out to the DOM
  */
 function updateUI() {
+
+	//Update our cash on hand
+	$("#currentCash").text(myUser.cash);
+
+	//Give visual feedback if we run out of money
+	var $el = $(".buy");
+	if(!canAffordFruit()) {
+		$el.removeClass("btn-primary");
+		$el.addClass("btn-danger");
+	} else {
+		$el.removeClass("btn-danger");
+		$el.addClass("btn-primary");
+	}
+
 	//Get our fruits
 	var apple = findFruit("Apple");
 	var orange = findFruit("Orange");
@@ -156,28 +207,28 @@ function updateUI() {
 	var appleItem = myUser.getInventoryItem(apple);
 	if(appleItem != null) {
 		$("#avgApplePrice").text(myUser.getAvgPrice(apple));
-		$("#appleBadge").text(appleItem.history.length);
+		$("#appleBadge").text(appleItem.quantity);
 	}
 	$("#applePrice").text(apple.currentPrice);
 
 	var orangeItem = myUser.getInventoryItem(orange);
 	if(orangeItem != null) {
 		$("#avgOrangePrice").text(myUser.getAvgPrice(orange));
-		$("#orangeBadge").text(orangeItem.history.length);
+		$("#orangeBadge").text(orangeItem.quantity);
 	}
 	$("#orangePrice").text(orange.currentPrice);
 
 	var bananaItem = myUser.getInventoryItem(banana);
 	if(bananaItem != null) {
 		$("#avgBananaPrice").text(myUser.getAvgPrice(banana));
-		$("#bananaBadge").text(bananaItem.history.length);
+		$("#bananaBadge").text(bananaItem.quantity);
 	}
 	$("#bananaPrice").text(apple.currentPrice);
 
 	var pearItem = myUser.getInventoryItem(pear);
 	if(pearItem != null) {
 		$("#avgPearPrice").text(myUser.getAvgPrice(pear));
-		$("#pearBadge").text(pearItem.history.length);
+		$("#pearBadge").text(pearItem.quantity);
 	}
 	$("#pearPrice").text(pear.currentPrice);
 
